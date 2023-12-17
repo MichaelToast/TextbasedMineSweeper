b04ab74d17fff5ef4b29a79f08be137e73612980
MichaelToast <125412378+MichaelToast@users.noreply.github.com>
2023-12-17 22:58:20 +0000

    Adding in the basic values for the game board

--- main.c
+++ main.c
@@ -2,6 +2,13 @@
 #define X 9
 #define Y 9
 
+const int mineCount = 10;
+
+typedef struct node {
+  int value;
+  int shown;
+} Node;
+
 int main(void) {
   printf("Hopefully this works\n");
   return 0;
