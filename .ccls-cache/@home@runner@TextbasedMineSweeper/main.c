10db2feded6ab2f2e2d0c03cb66b5e5bf9cc5aba
MichaelToast <125412378+MichaelToast@users.noreply.github.com>
2023-12-19 00:15:18 +0000

    Started adding in basic outline of the game

--- .ccls-cache/@home@runner@TextbasedMineSweeper/main.c
+++ .ccls-cache/@home@runner@TextbasedMineSweeper/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include <time.h>
 #define X 9
 #define Y 9
@@ -17,9 +18,59 @@ void displayBoardValues(Node[][Y]);
 int main(void) {
   bool win = false;
   Node board[X][Y] = {0};
+  int userXvalue = -1;
+  int userYvalue = -1;
+  int flagCount = 0;
+  char flag = 0;
+
+  while (win == false && flagCount != mineCount) { 
+    system("clear");
+
+    if (flagCount == mineCount) {
+      // {Function to determine if game has been won}
+    }
+
+    printf("There are %d mines on the board\n", mineCount);
+
+    displayBoard(board);
+    while (userXvalue == -1 && userYvalue == -1) {
+      printf("Place 'F' at beggining of your input to place/remove flag\n");
+      printf("Enter X Y Coordiates: \n");
+      scanf("%c%d%d", &flag, &userXvalue, &userYvalue); 
+      //F 5 6 AND F5 6
+
+      if (userXvalue > X || userYvalue > Y || userXvalue < 0 || userYvalue < 0) {
+        printf("\033[1;31mINVALID COORDINATE \033[0m");
+        userXvalue = -1;
+        userYvalue = -1; 
+      }
+
+      if (flag == 'f' || flag == 'F') {
+        // {Function to place/remove flag}
+        flag = 0;
+      }
+
+      
+    }
+    
+    
+
+
+    
+  
+  }
+
+  
+  
+  
+  if (flag == 'f' || flag == 'F') { 
+    printf("You want to place a flag\n");
+  }
+
+  printf("You have given me the coordinates - X: %d and Y: %d\n", userXvalue, userYvalue);
 
   displayBoard(board);
-  displayBoardValues(board);
+  //displayBoardValues(board);
   printf("Hopefully this works\n");
   return 0;
 }
